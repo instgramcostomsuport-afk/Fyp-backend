d22de141d0ae8b15c41b5a83f3d00f3db7516c22
instgramcostomsuport-afk <instgramcostomsuport@gmail.com>
2026-04-15 21:50:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -209,5 +209,5 @@ async def recommend(
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 7860))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
